463d93dbf31bb2561d10aaf59666dadfbcf96cb2
Anton Panchenko <nixegend@gmail.com>
2024-07-12 18:34:08 +0300

    add preloader

--- src/app/[locale]/contacts/loading.tsx
+++ src/app/[locale]/contacts/loading.tsx
@@ -1,5 +1,5 @@
 export default function Loading() {
   return (
-    <p>Loading global...</p>
+    <p>Loading Contacts...</p>
   );
 }
--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -2,6 +2,7 @@ import dynamic from 'next/dynamic';
 
 const DashboardApp = dynamic(() => import('../../../DashboardApp'), {
   ssr: false,
+  loading: () => <p>Loading DashboardApp...</p>,
 });
 
 export default function Dashboard() {
